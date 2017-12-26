567af8bd03acc9d3104a265a6b723237b4194778
Travis Rails <donghuanhe@gmail.com>
2017-12-26 06:35:41 +0000

    add and remove friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, :controllers =>{:registrations => "user/registrations"}
   
   root to: 'welcome#index'
+  resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
   
   get 'my_portfolio', to: "users#my_portfolio"
-  
-  get 'search_stocks', to: 'stocks#search'
+  get 'search_stocks', to: "stocks#search"
+  get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: "users#search"
+ post 'add_friend', to: "users#add_friend"
   
 end
